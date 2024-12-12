4a9764e52d44170393b51cd8df37c444d8dab45e
Leonardo-Corazzini <leocora10@gmail.com>
2024-12-12 16:27:32 +0100

    add language img

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,12 @@
 import { useContext } from "react";
 import GlobalContext from "../context/GlobalContext";
+const language = {
+    it: 'icons8-circolare-italia-48.png',
+    en: 'icons8-circolare-della-gran-bretagna-48.png',
+    de: 'icons8-circolare-tedesca-48.png',
+    fr: 'icons8-francia-circolare-48.png',
+    es: 'icons8-spagna-circolare-48.png'
+}
 
 
 export default function Main() {
@@ -9,7 +16,7 @@ export default function Main() {
             <ol>
                 <li>{title}</li>
                 <li>{original_title}</li>
-                <li>{original_language}</li>
+                <li><img src={language[original_language]} alt="" /></li>
                 <li>{vote_count}</li>
             </ol>
         </div>)

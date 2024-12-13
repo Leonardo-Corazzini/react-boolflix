74e28b2dd1e472260997259b43180e6dc0afe1ea
Leonardo-Corazzini <leocora10@gmail.com>
2024-12-13 14:20:57 +0100

    add starvote

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,18 +6,29 @@ const language = {
     es: 'icons8-spagna-circolare-48.png'
 }
 import { IMAGE_URI } from "../../config"
-// import ReactDOM from 'react-dom'
-// import { faStar } from '@fortawesome/free-solid-svg-icons'
-// import { faStar } from '@fortawesome/free-regular-svg-icons'
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as fullStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons'
 
 
 export default function Card({ search }) {
     const { title, original_title, name, original_name, original_language, vote_average, poster_path } = search
     const starVote = Math.round(vote_average / 2)
-    function starCalculator(vote) {
+    const emptyVote = 5 - starVote
+    function starArray(num1, num2) {
+        const starArray = []
+        for (let i = 0; i < num1; i++) {
+            starArray.push(fullStar)
 
+        }
+        for (let i = 0; i < num2; i++) {
 
+            starArray.push(emptyStar)
+        }
+        return starArray
     }
+    // let starArray = new Array(starVote).fill('star');
     return (
         <div>
             <div>
@@ -26,7 +37,7 @@ export default function Card({ search }) {
                     <li><img src={`${IMAGE_URI}${poster_path}`} alt="" /></li>
                     <li>{original_title || original_name}</li>
                     <li><img src={language[original_language]} alt="" /></li>
-                    <li>{starVote}</li>
+                    <li>{starArray(starVote, emptyVote).map((star, i) => <span key={i}> <FontAwesomeIcon icon={star} /></span>)}</li>
                 </ol>
             </div>
         </div>
